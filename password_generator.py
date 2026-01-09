6a2dd7ecd63be94224d991e8ed0ea73d6ca527dc
Haseena <akuthotapallihaseena@gmail.com>
2026-01-09 11:33:25 +0530

    password_generator.py

--- password_generator.py
+++ password_generator.py
@@ -3,10 +3,23 @@ import random
 import string
 
 class PasswordGenerator:
-    def __init__(self, length):
+    def __init__(self, length, use_letters, use_numbers, use_symbols):
         self.length = length
+        self.use_letters = use_letters
+        self.use_numbers = use_numbers
+        self.use_symbols = use_symbols
 
     def generate(self):
-        characters = string.ascii_letters + string.digits + string.punctuation
-        password = ''.join(random.choice(characters) for _ in range(self.length))
-        return password
+        characters = ""
+
+        if self.use_letters:
+            characters += string.ascii_letters
+        if self.use_numbers:
+            characters += string.digits
+        if self.use_symbols:
+            characters += string.punctuation
+
+        if not characters:
+            raise ValueError("Select at least one character type")
+
+        return ''.join(random.choice(characters) for _ in range(self.length))
